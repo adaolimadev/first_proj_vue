e50902e117224687636e7e985524e2480076e107
Adão Lima <adaognr.lima@outlook.com>
2023-03-09 10:27:17 -0300

    first commi

--- src/components/info.vue
+++ src/components/info.vue
@@ -1,20 +1,23 @@
 <template>
+    
     <div>
+        <Picture />
         <p v-if="esta_trabalhando">Estou trabalhando no momento.</p>
         <p v-else>Estou a procura de oportunidades</p>
         <p>utilizo as seguintes tecnologias:</p>
         <ul>
             <li>PHP</li>
             <li>Laravel</li>
-            <li>Python</li>
+            <li>VueJS</li>
+            <li>Docker</li>
+            <li>Git / GitHub</li>
         </ul>
+        <p>Para Acessar meu github <a v-bind:href="meu_link" target="_blank">clique aqui</a></p>
         <div>
-            <button @click="mostrar_email = !mostrar_email">Mostrar Email</button>
+            <button @click="showEmail">{{textoBotao}}</button>
         </div>
         <p v-show="mostrar_email">Seu email é {{ email }}</p>
-
-        <p>Para Acessar meu github <a v-bind:href="meu_link" target="_blank">clique aqui</a></p>
-        <Picture />
+        
     </div>
 </template>
 
@@ -31,12 +34,19 @@ export default {
             esta_trabalhando: false,
             meu_link: 'https://github.com/snakeg0d',
             email: 'adao@balfex.com.br',
-            mostrar_email: false
+            mostrar_email: false,
+            textoBotao: 'Mostrar Email'
         }
     },
     methods:{
         showEmail(){
             this.mostrar_email = !this.mostrar_email
+            if(!this.mostrar_email){
+                this.textoBotao = 'Mostrar e-mail'
+            }else{
+                this.textoBotao = 'Ocultar e-mail'
+
+            }
         }
     }
 }
